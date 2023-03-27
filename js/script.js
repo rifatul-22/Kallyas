243f6db30da81556d597f903e2b0ef9a1084c5ec
rifatul-22 <rifatulislam455@gmail.com>
2023-03-27 17:48:35 +0600

    v 1.0.2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(function(){
-    alert()
+    
+    // Movie Serial activation
+    $('.serial_movie_center').slick({
+        centerMode: true,
+        centerPadding: '80px',
+        slidesToShow: 5,
+        prevArrow:$('.serial_movie_center_icon_btn.prev'),
+        nextArrow:$('.serial_movie_center_icon_btn.next')
+      });
+});
+
+new VenoBox({
+  spinner: 'plane',
 });
