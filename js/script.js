3dc4c9fe205df83c4914712c807f0226a0667c94
rifatul-22 <rifatulislam455@gmail.com>
2023-03-30 03:34:09 +0600

    v 1.0.4

--- js/script.js
+++ js/script.js
@@ -8,6 +8,13 @@ $(function(){
         prevArrow:$('.serial_movie_center_icon_btn.prev'),
         nextArrow:$('.serial_movie_center_icon_btn.next')
       });
+    // serial filter activation
+    $('.serial_filter').filterizr();
+
+    $('.serial_filter_content li').on('click', function(){
+        $(this).addClass('f_active')
+        $(this).siblings().removeClass('f_active')
+    })
 });
 
 new VenoBox({
